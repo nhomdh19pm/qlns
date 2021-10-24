1c9ea35acb39f48c4557a601ae197791f0dd1580
hungnguyen <YOUR_EMAILnthung_19pm@student.agu.edu.vn>
2021-10-24 13:11:09 +0700

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,115 @@
-@extends('layouts.app')
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Đăng nhập hệ thống </title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="public/login/images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/login/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/login/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/login/vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="public/login/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/login/vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="public/login/css/util.css">
+	<link rel="stylesheet" type="text/css" href="public/login/css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="public/login/images/img-01.png" alt="IMG">
+				</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+				<form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
+				@csrf
+					<span class="login100-form-title">
+						Login on Company
+					</span>
 
-                        <div class="form-group">
-                                <input type="text"
-                                    class="form-control{{ $errors->has('email') || $errors->has('username') ? ' is-invalid' : '' }}"
-                                    id="email" name="email" value="{{ old('email') }}"
-                                    placeholder="Email hoặc Tên đăng nhập" required />
-                                @if ($errors->has('email') || $errors->has('username'))
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email" id="email" name="email" value="{{ old('email') }}" placeholder="Email or Username">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+						@if ($errors->has('email') || $errors->has('username'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>
                                             {{ empty($errors->first('email')) ? $errors->first('username') : $errors->first('email') }}
                                         </strong>
                                     </span>
-                                @endif
-                            </div>
+                         @endif
+					</div>
 
-                            <div class="form-group">
-                                <input type="password" class="form-control @error('password') is-invalid @enderror"
-                                    id="password" name="password" Placeholder="Password" required />
-                                @error('password')
+					<div class="wrap-input100 validate-input @error('password') is-invalid @enderror" data-validate = "Password is required">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+						@error('password')
                                     <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
-                                @enderror
+                        @enderror
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
 
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
-                            </div>
-                            <div class="form-group d-md-flex">
-                                <div class="w-50 text-md-left">
-                                    @if (Route::has('login'))
-                                        <a class="underline text-sm text-gray-600 hover:text-gray-900"
-                                            href="{{ route('register') }}">
-                                            Đăng ký
-                                        </a>
-                                    @endif
-                                </div>
-                                <div class="w-50 text-md-right">
-                                    @if (Route::has('password.request'))
-                                        <a class="underline text-sm text-gray-600 hover:text-gray-900"
-                                            href="{{ route('password.request') }}">
-                                            Quên Mât Khẩu
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+					<div class="text-center p-t-12">
+						<span class="txt1">
+							Forgot
+						</span>
+						<a class="txt2" href="#">
+							Username /
+						</a>
+						<a class="txt2" href="#">
+							Password?
+						</a>
+					</div>
+
+					<div class="text-center p-t-136">
+						<a class="txt2" href="#">
+							Create your Account
+							<i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+<!--===============================================================================================-->	
+	<script src="public/login/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="public/login/vendor/bootstrap/js/popper.js"></script>
+	<script src="public/login/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="public/login/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="public/login/vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+<!--===============================================================================================-->
+	<script src="public/login/js/main.js"></script>
+
+</body>
+</html>
